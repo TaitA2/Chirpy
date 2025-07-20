5793174610611a0e934ac11e85bf68db28a5509e
Adam PARCH <adam.tait617@gmail.com>
2025-07-20 12:47:19 +0100

    reformat main

--- main.go
+++ main.go
@@ -7,26 +7,19 @@ import (
 )
 
 func main() {
-	serveMux := http.NewServeMux()
-	server := &http.Server{Handler: serveMux, Addr: ":8080"}
 
 	var apiCfg apiConfig
+
+	serveMux := http.NewServeMux()
+
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	serveMux.HandleFunc("/healthz/", handlerReadiness)
-	serveMux.HandleFunc("/metrics/", func(writer http.ResponseWriter, req *http.Request) {
-		writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		writer.WriteHeader(200)
-		writer.Write(fmt.Appendf(nil, "Hits: %v", apiCfg.fileserverHits.Load()))
-	})
+	serveMux.HandleFunc("/metrics/", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("/reset/", apiCfg.handlerReset)
 
-	serveMux.HandleFunc("/reset/", func(writer http.ResponseWriter, req *http.Request) {
-		writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		writer.WriteHeader(200)
-		writer.Write([]byte("OK"))
-		apiCfg.fileserverHits.Swap(0)
-	})
-
+	server := &http.Server{Handler: serveMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
 
@@ -45,4 +38,17 @@ func handlerReadiness(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (apiCfg *apiConfig) handlerMetrics(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write(fmt.Appendf(nil, "Hits: %v", apiCfg.fileserverHits.Load()))
+}
+
+func (apiCfg *apiConfig) handlerReset(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
+	apiCfg.fileserverHits.Swap(0)
 }
