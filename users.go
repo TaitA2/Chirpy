c35cc0802f4f7c567868ed524f3579adf645d004
Adam PARCH <adam.tait617@gmail.com>
2025-07-24 11:47:55 +0100

    remove redundant user struct def

--- users.go
+++ users.go
@@ -4,17 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/google/uuid"
 )
-
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
-}
 
 func (apiCfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
